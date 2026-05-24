229d1a9143a192024d37eb758c789fd424b95149
aditya raj chauhan <166774675+aditya-raj-chauhan@users.noreply.github.com>
2026-05-24 14:08:34 +0530

    creating resume templates

---
+++ src/main/java/com/devotee/resume/init/Controller/TemplatesController.java
@@ -0,0 +1,29 @@
+package com.devotee.resume.init.Controller;
+
+import com.devotee.resume.init.service.TemplatesService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/templates")
+@RequiredArgsConstructor
+@Slf4j
+public class TemplatesController {
+    private final TemplatesService templatesService;
+    @GetMapping
+    public ResponseEntity<?>getTemplates(Authentication authentication){
+        //call the service method
+       Map<String,String>response= templatesService.getTemplates(authentication.getPrincipal());
+
+        //return the response
+        return ResponseEntity.ok(response);
+
+    }
+}
